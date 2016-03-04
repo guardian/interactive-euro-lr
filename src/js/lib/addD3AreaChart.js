668fa99e26c2c8d1a9f49daa9eb80241d906af9b
cfen <chris.fenn@guardian.co.uk>
2016-03-04 13:23:13 +0000

    adding slider to graph

--- src/js/lib/addD3AreaChart.js
+++ src/js/lib/addD3AreaChart.js
@@ -2,21 +2,20 @@ import d3 from 'd3'
 
 export default function addD3AreaChart(data){
 
-    var targetDiv = document.getElementById('areaChartHolder');
-        targetDiv.innerHTML = " ";
-     
-
+        var targetDiv = document.getElementById('areaChartHolder');
+            targetDiv.innerHTML = " ";
+   
         var margin = {top: 20, right: 20, bottom: 30, left: 50},
-            width = 960 - margin.left - margin.right,
-            height = 240 - margin.top - margin.bottom;
+            width = 240 - margin.left - margin.right,
+            height = 620 - margin.top - margin.bottom;
 
         var parseDate = d3.time.format("%Y%m%d").parse;
 
-        var x = d3.time.scale()
-            .range([0, width]);
+        var y = d3.time.scale()
+            .range([0, height]);
 
-        var y = d3.scale.linear()
-            .range([height, 0]);
+        var x = d3.scale.linear()
+            .range([width, 0]);
 
         var xAxis = d3.svg.axis()
             .scale(x)
@@ -28,35 +27,35 @@ export default function addD3AreaChart(data){
 
         var line = d3.svg.line()
             .interpolate("basis")
-            .x(function(d) { return x(d.date); })
-            .y(function(d) { return y(d.temperature); });
+            .x(function(d) { return x(d.temperature); })
+            .y(function(d) { return y(d.date); });
 
         var svg = d3.select(targetDiv).append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
+
           .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
         
          data.forEach(function(d) {
-
             console.log(d)
             d.date = parseDate(d.compDate);
             d.temperature = + d.lrCount;
           });
 
-          x.domain([data[0].date, data[data.length - 1].date]);
-          y.domain(d3.extent(data, function(d) { return d.temperature; }));
+          x.domain([d3.max(data, function(d) { return d.temperature; }), -16]);
+          y.domain([data[data.length - 1].date, data[0].date]);
 
           svg.append("linearGradient")
               .attr("id", "temperature-gradient")
               .attr("gradientUnits", "userSpaceOnUse")
-              .attr("x1", 0).attr("y1", y(-7))
-              .attr("x2", 0).attr("y2", y(16))
+              .attr("x1", -16).attr("y1", y(0))
+              .attr("x2", 16).attr("y2", y(0))
             .selectAll("stop")
               .data([
                 {offset: "0%", color: "red"},
                 {offset: "50%", color: "orange"},
-                {offset: "100%", color: "blue"}
+                {offset: "100%", color: "navy"}
               ])
             .enter().append("stop")
               .attr("offset", function(d) { return d.offset; })
@@ -75,13 +74,55 @@ export default function addD3AreaChart(data){
               .attr("y", 6)
               .attr("dy", ".71em")
               .style("text-anchor", "end")
-              .text("Temperature (ºF)");
+              .text("Date");
 
           svg.append("path")
               .datum(data)
               .attr("class", "line")
               .attr("d", line);
-       
 
+          var infobox = svg.append("g")
+              .attr("class", "infobox")
+              .attr("id", "infoBox");
 
+              infobox.append("rect")
+              .attr("width", width)
+              .attr("x",-10)
+              .attr("height", 20);
+
+              infobox.append("line")
+              .attr("x1", width)
+              .attr("x2", 0);
+
+              var focus = svg.append("g")
+              .attr("class", "focus");
+
+              focus.append("circle")
+              .attr("r", 2);
+
+              infobox.append("text")
+              .attr("x", -6)
+              .attr("y", -9);
+
+              svg.append("rect")
+                .attr("class", "svg-overlay")
+                .attr("width", width)
+                .attr("height", height)
+                .on('click', function(e) { console.log(' clicking area', e); })
+                .on("mousemove", mousemove());
+
+                
+
+      function mousemove() {
+          var d = offsets[Math.round((x.invert(d3.mouse(this)[0]) - start) / step)];
+          focus.select("circle").attr("transform", "translate(" + x(d[0]) + "," + y(d[1]) + ")");
+          focus.select(".x").attr("transform", "translate(" + x(d[0]) + ",0)");
+          focus.select(".y").attr("transform", "translate(0," + y(d[1]) + ")");
+          svg.selectAll(".x.axis path").style("fill-opacity", Math.random()); // XXX Chrome redraw bug
+        }
 }
+
+
+
+
+
